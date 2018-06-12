70d7fd0d0a27aff815488e71bdde26c546126cf8
Felipe Oliveira <felipe.o.silva18@gmail.com>
2018-06-12 00:41:54 -0300

    add test to not create books with isbn existent

--- src/mutation/test/IntroduceBook.spec.js
+++ src/mutation/test/IntroduceBook.spec.js
@@ -5,6 +5,7 @@ import {
   connectDatabase,
   removeAllDocuments,
   disconnectDatabase,
+  createBook,
 } from '../../test/helpers';
 import schema from '../../schema';
 import fixture from './fixture.json';
@@ -16,6 +17,40 @@ beforeEach(removeAllDocuments);
 afterAll(disconnectDatabase);
 
 describe('Mutation IntroduceBook', () => {
+  it('should not create with an existing isbn', async () => {
+    await createBook(fixture.book);
+
+    const query = `
+      mutation IntroduceBook($input: IntroduceBookInput!)  {
+        IntroduceBook(input: $input) {
+          book {
+            title
+            author
+            isbn
+          }
+          error
+          clientMutationId
+        }
+      }
+    `;
+
+    const rootValue = {};
+    const variables = {
+      input: {
+        ...fixture.book,
+        clientMutationId: fixture.clientMutationId,
+      },
+    };
+
+    const context = getContext();
+    const result = await graphql(schema, query, rootValue, context, variables);
+    const { book, error, clientMutationId } = result.data.IntroduceBook;
+
+    expect(book).toEqual(null);
+    expect(clientMutationId).toEqual(fixture.clientMutationId);
+    expect(error).toEqual('ISBN_ALREADY_REGISTRED');
+  });
+
   it('should create a new book when parameters are valid', async () => {
     const query = `
       mutation IntroduceBook($input: IntroduceBookInput!)  {
@@ -32,15 +67,20 @@ describe('Mutation IntroduceBook', () => {
     `;
 
     const rootValue = {};
-    const variables = { input: fixture };
-    const context = getContext();
+    const variables = {
+      input: {
+        ...fixture.book,
+        clientMutationId: fixture.clientMutationId,
+      },
+    };
 
+    const context = getContext();
     const result = await graphql(schema, query, rootValue, context, variables);
     const { book, error, clientMutationId } = result.data.IntroduceBook;
 
-    expect(book.title).toEqual(fixture.title);
-    expect(book.author).toEqual(fixture.author);
-    expect(book.isbn).toEqual(fixture.isbn);
+    expect(book.title).toEqual(fixture.book.title);
+    expect(book.author).toEqual(fixture.book.author);
+    expect(book.isbn).toEqual(fixture.book.isbn);
     expect(error).toEqual(null);
     expect(clientMutationId).toEqual(fixture.clientMutationId);
   });
